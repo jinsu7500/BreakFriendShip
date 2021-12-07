f4fbd682407a2f4fb80df5f774c7a1e2ae47a89b
shinmc12 <84845045+shinmc12@users.noreply.github.com>
2021-12-07 19:28:37 +0900

    6라운드

--- Assets/Scripts/GhostScript.cs
+++ Assets/Scripts/GhostScript.cs
@@ -10,52 +10,62 @@ public class GhostScript : MonoBehaviourPunCallbacks
     //int FruitCount = 0;
     public float speed;
     public float distance;
+    public GameObject PlayerSpawn;
+    public GameObject PlayerSpawn2;
     public GameObject player;
     public PhotonView PV;
     public Rigidbody2D RB;
+    public GameObject GhostTest;
 
-    public bool isTrigger = false;
 
-    private string name = "";
+    private string names = "";
     void Start()
     {
-
     }
     void Update()
     {
+        names = GameObject.Find("R6GhostTrigger").GetComponent<GhostTrigger>().names;
 
     }
 
     public void GhostMove()
     {
-        int i = 0;
-        if (isTrigger)
+        if (names != "")
         {
+            int i = 0;
+
             for (i = 0; i < player.transform.childCount; i++)
             {
-                if (player.transform.GetChild(i).transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text == name) break;
+                if (player.transform.GetChild(i).transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text == names) break;
             }
 
             float x = player.transform.GetChild(i).position.x;
             float y = player.transform.GetChild(i).position.y;
             Vector2 vec = new Vector2(x, y);
             transform.position = Vector2.MoveTowards(transform.position, vec, 0.015f);
+
+            PV.RPC("Syn_update_move", RpcTarget.All);
         }
-        PV.RPC("Syn_update_move", RpcTarget.All);
     }
 
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("충돌");
-
         if (collision.tag == "Player")
         {
-            Debug.Log(collision.gameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text);
-            isTrigger = true;
-            name = collision.gameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text;
-            //PlayerPosition = collision.transform.position;
-            //FruitCount++;
+            int index = 0;
+            Debug.Log("dieArea 진입!");
+            for (index = 0; index < player.transform.childCount; index++)
+            {
+                if (player.transform.GetChild(index).transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text == PhotonNetwork.LocalPlayer.NickName)
+                {
+                    break;
+                }
+            }
+
+            player.transform.GetChild(index).transform.position = new Vector3(PlayerSpawn.transform.position.x, PlayerSpawn.transform.position.y, PlayerSpawn.transform.position.z);
+            GhostTest.transform.position = new Vector3(PlayerSpawn2.transform.position.x, PlayerSpawn2.transform.position.y, PlayerSpawn2.transform.position.z);
+
         }
 
     }
@@ -63,13 +73,15 @@ public class GhostScript : MonoBehaviourPunCallbacks
     [PunRPC]
     public void Syn_update_move()
     {
-        int i = 0;
-        if (isTrigger)
+        if (names != null)
         {
+            int i = 0;
+
             for (i = 0; i < player.transform.childCount; i++)
             {
-                if (player.transform.GetChild(i).transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text == name) break;
+                if (player.transform.GetChild(i).transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text == names) break;
             }
+
             float x = player.transform.GetChild(i).position.x;
             float y = player.transform.GetChild(i).position.y;
             Vector2 vec = new Vector2(x, y);
