ab78720aae7b65e735c05b191e95527954d66f7f
Hong Boem Sun <qjatjs123123@naver.com>
2021-12-10 22:28:45 +0900

    라운드 4 점프, 이동 수정

--- Assets/Scripts/Together_Move.cs
+++ Assets/Scripts/Together_Move.cs
@@ -9,29 +9,37 @@ public class Together_Move : MonoBehaviour
     public GameObject apple;
     public GameObject position;
     public PhotonView PV;
+    public float player_count;
 
     [SerializeField]
     private float moveSpeed = 0.3f;
     [SerializeField]
     private float jumpSpeed = 0.3f;
 
+    //int player_count = 0;
 
+    void Start()
+    {
+        player_count = GameObject.Find("Player").transform.childCount;
+    }
 
     public void Move_to_Right()
     {
+
         float x = apple.transform.position.x + 0.01f;
         float y = apple.transform.position.y;
         Vector2 vec = new Vector2(x, y);
-        transform.position = Vector2.MoveTowards(transform.position, vec, moveSpeed);
+        transform.position = Vector2.MoveTowards(transform.position, vec, moveSpeed / player_count);
         PV.RPC("Right_Update", RpcTarget.Others);
 
     }
     public void Move_to_Left()
     {
+
         float x = apple.transform.position.x - 0.01f;
         float y = apple.transform.position.y;
         Vector2 vec = new Vector2(x, y);
-        transform.position = Vector2.MoveTowards(transform.position, vec, moveSpeed);
+        transform.position = Vector2.MoveTowards(transform.position, vec, moveSpeed / player_count);
         PV.RPC("Left_Update", RpcTarget.Others);
     }
     public void Move_to_Up()
@@ -39,7 +47,7 @@ public class Together_Move : MonoBehaviour
         float x = apple.transform.position.x;
         float y = apple.transform.position.y + 0.005f;
         Vector2 vec = new Vector2(x, y);
-        transform.position = Vector2.MoveTowards(transform.position, vec, jumpSpeed);
+        transform.position = Vector2.MoveTowards(transform.position, vec, jumpSpeed / player_count);
         PV.RPC("UP_Update", RpcTarget.Others);
     }
 
@@ -49,7 +57,7 @@ public class Together_Move : MonoBehaviour
         float x = apple.transform.position.x + 0.01f;
         float y = apple.transform.position.y;
         Vector2 vec = new Vector2(x, y);
-        transform.position = Vector2.MoveTowards(transform.position, vec, moveSpeed);
+        transform.position = Vector2.MoveTowards(transform.position, vec, moveSpeed / player_count);
     }
 
     [PunRPC]
@@ -58,7 +66,7 @@ public class Together_Move : MonoBehaviour
         float x = apple.transform.position.x - 0.01f;
         float y = apple.transform.position.y;
         Vector2 vec = new Vector2(x, y);
-        transform.position = Vector2.MoveTowards(transform.position, vec, moveSpeed);
+        transform.position = Vector2.MoveTowards(transform.position, vec, moveSpeed / player_count);
     }
 
     [PunRPC]
@@ -67,7 +75,7 @@ public class Together_Move : MonoBehaviour
         float x = apple.transform.position.x;
         float y = apple.transform.position.y + 0.005f;
         Vector2 vec = new Vector2(x, y);
-        transform.position = Vector2.MoveTowards(transform.position, vec, jumpSpeed);
+        transform.position = Vector2.MoveTowards(transform.position, vec, jumpSpeed / player_count);
     }
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -75,7 +83,7 @@ public class Together_Move : MonoBehaviour
         if (other.gameObject.name == "Tilemap")
         {
             apple.transform.position = new Vector3(position.transform.position.x, position.transform.position.y, position.transform.position.z);
-            
+
         }
     }
 }
